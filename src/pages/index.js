87811659accdba1f7dc80d39f4fb1f2d4cb86ede
RosemaryLy <rosey.ly@hotmail.com>
2021-04-06 11:15:00 -0400

    add in video for jumbotron

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Container, Button, Jumbotron } from "react-bootstrap"
 import Layout from "../components/layout"
+import cupcakeCut from "../assets/cupcakecut.mp4"
 
 export default () => (
   <Layout>
     <Container fluid>
       <Jumbotron fluid>
-        <h1>Hello, world!</h1>
-        <video src="../assets/cucpakecute.mp4" muted loop autoPlay></video>
+        <video muted loop autoPlay>
+          <source src={cupcakeCut} type="video/mp4" />
+        </video>
         <p>
           <Button variant="primary">Sample Button</Button>
         </p>
