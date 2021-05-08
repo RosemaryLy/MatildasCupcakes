10b2356f4274abf78cf018f496a42765122d1eba
RosemaryLy <rosey.ly@hotmail.com>
2021-05-08 17:15:41 -0400

    lorum ipsum filler

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -7,17 +7,23 @@ export default function Section(){
     return (
       <Container fluid>
       <Row>
-        <Col>
+        <Col >
         <div className="text-center">
           <Image src={vanillacupcake} roundedCircle width="500" height="500" />
           </div>
         </Col>
-        <Col style={{backgroundColor:'pink'}}>
-          2 of 2</Col>
+        <Col className="align-items-center" style={{backgroundColor:'pink'}}>
+      
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          
+          </Col>
       </Row>
       <Row>
         <Col style={{backgroundColor:'gold'}}>
-          1 of 3</Col>
+        <div className="text-center">
+          1 of 3
+          </div>
+          </Col>
         <Col>
         <div className="text-center">
           <Image src={strawberrycupcake} roundedCircle width="500" height="500"/>
